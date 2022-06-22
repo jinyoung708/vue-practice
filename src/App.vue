6ba4f19f615add1514ced5abbe56788916bebff3
HOME\oh.jinyoung <oh_jinyoung@radcns.com>
2022-06-22 12:52:57 +0900

    upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    test
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="scss">
+@import url('@/assets/scss/common.scss');
+</style>
